d865dce0226203f6b789e34e7dd09dd2d1c1e743
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-15 17:34:21 +0000

    This lecture we learn what is conditional and loops and how to use it

---
+++ dsa/lecture2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Conditional
+    // Basic Program on if else Condition
+    // int num;
+    // cout<<"Enter a number:";
+    // cin>>num;
+    // if(num%2==0)
+    // cout<<"Number is Even"<<endl;
+    // else
+    // cout<<"Number is Odd"<<endl;
+
+    //Loops
+    // 11 time print your name
+    for(int i=0;i<11;i++){
+        cout<<"Nilesh Pandey" << endl;
+    }
+    //19 ka table
+    for(int j=1;j<=10;j++){
+        cout<<19*j<<endl;
+    }
+    // //1 to 100 even number print
+    for(int e=0;e<100;e++){
+        if(e%2==0){
+            cout<<e<<" ";
+        }
+    }
+}
