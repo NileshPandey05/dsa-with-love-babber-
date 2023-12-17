60360e8304c6542e6541745c299669c7821327d9
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-17 15:25:08 +0000

    Question on Pattern

--- dsa/homework_pattern.cpp
+++ dsa/homework_pattern.cpp
@@ -28,19 +28,55 @@ int main(){
     //     }
     //     cout<<endl;
     // }
-    // full prramid
-    for(int i=0;i<n;i++){
-    int k=0;
-    for(int j=0;j<((2*n)-1);j++){
-        if(j<n-i-1){
-            cout<<" ";
-        }else if(k<2*i+1){
-            cout<<("*");
-            k++;
-        }else{
-            cout<<" ";
-        }
+   // full pramid
+    // for(int row=0;row<n;row++){
+    //     for(int col=0;col<n-row-1;col++){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0;col<row+1;col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+   // numeric rectangle
+//    int a=1;
+//     for(int row=0;row<n;row++){
+//         for(int col=0;col<n;col++){
+//             cout<<a;
+//             a=a+1;
+//         }
+//         cout<<endl;
+//     }
+//right angled triangle
+// for(int row=0;row<n;row++){
+//     for(int col=0;col<row+1;col++){
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+//}
+//numeric pramids
+// int count=1;
+//  for(int row=0;row<n;row++){
+//     for(int col=0;col<row+1;col++){
+//         cout<<count;
+//         count=count+1;
+//     }
+//     cout<<endl;
+//  }
+//  for(int row=0;row<n;row++){
+//     for(int col=0;col<row+1;col++){
+//         cout<<row;
+//     }
+//     cout<<endl;
+//  }
+ int row,col;
+ 
+ for( row=1;row<=n;row++){
+    int value=row;
+    for(col=1;col<=row;col++){
+        cout<<value;
+        value=value+1;
     }
     cout<<endl;
-}
+ }
 }
