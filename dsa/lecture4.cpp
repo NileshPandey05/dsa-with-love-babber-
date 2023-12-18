42be2b9d7ce95211fe15ee1b43210f520f9ef24e
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-18 06:24:00 +0000

    Bitwise operater,Pre/Post Increment and Decrement,Breake and Continue and what is global and local variable

--- dsa/lecture4.cpp
+++ dsa/lecture4.cpp
@@ -9,6 +9,21 @@ int main(){
     bool num=1;
     cout<<~num<<endl;
     cout<<~(num)<<endl;
-    cout<<(a<<-1)<<endl;
+    cout<<(a<<1)<<endl;
+    // Pre/Post Increment and Decrement
+    cout<<((++a)*(a++))<<endl;
+    cout<<a<<endl;
+    //Breake and Continue
+    for(int i=0;i<5;i++){
+        cout<<i<<endl;
+        if(i==2){
+            break;  // This will stop the loop when it reaches to value of '2'
+        }
+    }
+    for(int i=0;i<5;i++){
+        cout<<"Hello Jee"<<endl;
+        continue;
+        cout<<"Kaise ho saare"<<endl;
+    }
     return 0;
 }
