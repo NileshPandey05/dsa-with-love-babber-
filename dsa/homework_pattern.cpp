1280e05b2dad37b74ae3480f00458067ef72e9fb
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-18 17:16:26 +0000

    Pattern Question

--- dsa/homework_pattern.cpp
+++ dsa/homework_pattern.cpp
@@ -70,13 +70,62 @@ int main(){
 //     cout<<endl;
 //  }
  int row,col;
- 
- for( row=1;row<=n;row++){
-    int value=row;
-    for(col=1;col<=row;col++){
-        cout<<value;
-        value=value+1;
+ // full  pyramid
+//  for( row=0;row<n;row++){
+//     int value=row;
+//     for(col=0;col<(n-row-1);col++){
+//         cout<<"  ";
+//     }
+//     for(int col=1;col<(2*(row+1));col++){
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+//  }
+ //full inverted pyramid
+//  for( row=0;row<n;row++){
+//     int value=row;
+//     for(col=0;col<row;col++){
+//         cout<<"  ";
+//     }
+//     for(int col=1;col<(2*(n-row));col++){
+//          cout<<"* ";
+//     }
+//     cout<<endl;
+//  }
+    //Dimond pattern
+//    int n=num/2;
+//     for( row=0;row<n;row++){
+//     int value=row;
+//     for(col=0;col<(n-row-1);col++){
+//         cout<<"  ";
+//     }
+//     for(int col=1;col<(2*(row+1));col++){
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+//  }
+//  for( row=0;row<n;row++){
+//     int value=row;
+//     for(col=0;col<row;col++){
+//         cout<<"  ";
+//     }
+//     for(int col=1;col<(2*(n-row));col++){
+//          cout<<"* ";
+//     }
+//     cout<<endl;
+//  }
+ for(row=0;row<n;row++){
+    for(col=0;col<(n-row-1);col++){
+        cout <<" ";
+    }
+    for(col=0;col<(row+1);col++){
+        if(col==0||col==row+1-1)
+        {
+            cout << "* ";
+        }else{
+            cout << "  ";
+        }
     }
     cout<<endl;
- }
+}
 }
