f0f7576dabe67075d64e83cf9d898516c67f6a79
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-24 16:48:42 +0000

    patter

--- dsa/homework_pattern.cpp
+++ dsa/homework_pattern.cpp
@@ -114,5 +114,39 @@ int main(){
 //     }
 //     cout<<endl;
 //  }
-    //hollow full pyrami
+    //hollow full pyramid
+    // for(int row=0;row<5;row++){
+    //     for(int col=0;col<5-row-1;col++){
+    //         cout<<"  ";
+    //     }
+    //     for(int col=0;<(2*(row+1));col++){
+    //         if(row==0||row==5-1||col==0||col==5-1){
+    //             cout<<"* ";
+    //         }else{
+    //             cout<<" ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+    // solid half diamond
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++){
+        for(int col=0;col<(row+1);col++){
+            cout<<"* ";
+        }
+        for(int col=0;col<(n-row-1);col++){
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+    for(int row=0;row<n;row++){
+        for(int col=0;col<(n-row-1);col++){
+            cout<<"* ";
+        }
+        for(int col=0;col<(row+1);col++){
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
 }
