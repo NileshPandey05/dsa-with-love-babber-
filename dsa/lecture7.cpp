0c42e82b37239598ecc0a463b34246f3fd48d4c7
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-25 15:02:40 +0000

    Array

---
+++ dsa/lecture7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // We learn about Array
+    // what is array
+    // why we need array
+    int arr[10];
+    // address of array 
+    cout << arr <<endl;
+    cout<<sizeof(arr)<<endl;
+    int arra[5]={1,2,3,4,5};
+    int array[10]={1,2,3,4,5};
+    cout << array[2] <<endl;
+}
