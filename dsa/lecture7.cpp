51e90cdd08b1dc08e1e4b9c833434f78dbed839b
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-26 17:26:40 +0000

    Array

--- dsa/lecture7.cpp
+++ dsa/lecture7.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<limit.h>
 using namespace std;
 int main(){
     // We learn about Array
@@ -131,4 +132,21 @@ int main(){
     // for(int i=0;i<size;i++){
     //     cout << arr[i] << " ";
     // }
+
+    // Reverse an array by using three variable
+     int arr[8]={1,2,3,4,5,6,7,8};
+    int size =8;
+    
+    int start=0;
+    int end = size-1;
+    while(start<end){
+        int temp=arr[end];
+        arr[end]=arr[start];
+        arr[start]=temp;
+        start++;
+        end--;
+    }
+    for(int i=0;i<size;i++){
+        cout << arr[i] <<endl;
+    }
 }
