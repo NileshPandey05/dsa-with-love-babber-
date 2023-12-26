d1b8d6ac599e78af556993371474b5b4adbf8746
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-26 16:58:09 +0000

    Array

--- dsa/lecture7.cpp
+++ dsa/lecture7.cpp
@@ -37,4 +37,98 @@ int main(){
     // for(int i=0;i<5;i++){
     //     cout << arr[i] << " ";
     // }
+
+    // int arr[5]={1,3,5,7,8};
+    // int size = 5;
+    // cout << "Enter the key to find" << endl;
+    // int key;
+    // cin >> key
+    // if(find(arr,size,key)){
+    //     cout<< "Found" << endl;
+    // }else{
+    //     cout << "Not Found" <<endl;
+    // }
+
+
+    // count 0's and 1's
+    // int arr[]={0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+    // int size = 15;
+
+    // int numZero=0;
+    // int numOne=0;
+    // for(int i=0;i<size;i++){
+    //     // if zero found ,increment numZero
+    //     if(arr[i]==0){
+    //         numZero++;
+    //     }
+    //     // if One found ,increment numOne
+    //     if(arr[i]==1){
+    //         numOne++;
+    //     }
+    // }
+    // cout << "Number of Zero" << numZero << endl;
+    // cout << "Number of Ones" << numOne << endl;
+
+    //Maximum Number in an array
+    // int arr[]={2,4,6,1,3,7,9,12,56,43,21};
+    // int size=11;
+    // int max = INT_MIN;
+    // for(int i=0;i<size;i++){
+    //     if(arr[i]>max){
+    //         //found a number greater then max ,update maxi
+    //         max=arr[i];
+    //     }
+    // }
+    // cout << "Maximum number is" << max << endl;
+
+    //Find minimum number in an array
+    // int arr[]={2,4,6,1,3,7,9,12,56,43,21};
+    // int size=11;
+    // int max = INT_MIN;
+    // int min = INT_MAX;
+    // for(int i=0;i<size;i++){
+    //     if(arr[i]<min){
+    //         //found a number greater then max ,update maxi
+    //         min=arr[i];
+    //     }
+    // }
+    // cout << "Maximum number is" << min << endl;
+
+    // Extreme print in array
+    // int arr[8]={10,20,30,40,50,60,70,80};
+    // int size=8;
+    // int start=0;
+    // int end=size-1;
+
+    // while(true){
+    //     if(start>end)
+    //         break;
+    //     if(start==end)
+    //         cout << arr[start] << " ";
+    //     else{
+    //         cout << arr[start] << " ";
+    //         cout << arr[end] << " ";
+    //     }
+    //     start++;
+    //     end--;
+    // }
+ 
+    
+    // Reverse an array
+    // int arr[8]={10,20,30,40,50,60,70,80};
+    // int size=8;
+    // int start=0;
+    // int end=size-1; 
+    // while(start<=end){
+    //     //step 1
+    //     swap(arr[start],arr[end]);
+    //     // step 2
+    //     start++;
+    //     // step 3
+    //     end--;
+    // }
+    // //Printing array
+    // for(int i=0;i<size;i++){
+    //     cout << arr[i] << " ";
+    // }
 }
