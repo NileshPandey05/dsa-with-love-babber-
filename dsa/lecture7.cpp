9ba202ef40485f13524f098e49582116f91b1be5
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2023-12-26 12:02:22 +0000

    Array

--- dsa/lecture7.cpp
+++ dsa/lecture7.cpp
@@ -4,11 +4,37 @@ int main(){
     // We learn about Array
     // what is array
     // why we need array
-    int arr[10];
-    // address of array 
-    cout << arr <<endl;
-    cout<<sizeof(arr)<<endl;
-    int arra[5]={1,2,3,4,5};
-    int array[10]={1,2,3,4,5};
-    cout << array[2] <<endl;
+    // int arr[10];
+    // // address of array 
+    // cout << arr <<endl;
+    // cout<<sizeof(arr)<<endl;
+    // int arra[5]={1,2,3,4,5};
+    // int array[10]={1,2,3,4,5};
+    // cout << array[2] <<endl;
+    // int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    // for(int i=0;i<=10;i++){
+    //     cout << arr[i] << endl;
+    // }
+    // int arr[100];
+    // int n;
+    // cout<<"How many number you want to add in array" << endl;
+    // cin >> n;
+    // cout<<"Enter the number "<<endl;
+    
+    // for(int i=0;i<10;i++){
+    //     cin >> arr[i];
+    // }
+
+    // cout << "Double:" << endl;
+    // for(int i=0;i<10;i++){
+    //     cout << arr[i] << " ";
+    // }
+
+    // int arr[]={1,3,5,7,9};
+    // for(int i=0;i<5;i++){
+    //     arr[i]=1;
+    // }
+    // for(int i=0;i<5;i++){
+    //     cout << arr[i] << " ";
+    // }
 }
