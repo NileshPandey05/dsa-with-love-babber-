ecfe51d33f2882309a539b0c4477276ee3c5aa06
Nilesh Pandey <nileshpandey_ce_2021@ltce.in>
2024-01-08 02:41:27 +0530

    vector

--- dsa/lecture8.cpp
+++ dsa/lecture8.cpp
@@ -2,7 +2,13 @@
 #include<vector>
 using namespace std;
 
-
+int findUnique(vector<int> arr){
+  int ans =0;
+  for(int i=0;i<arr.size();i++){
+    ans = ans^arr[i];
+  }
+  return ans;
+}
 
 int main(){
 
